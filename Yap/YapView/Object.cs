607c370186b1ea11119ede0a66ffd6339e7d918e
yvan vander sanden <yvan@attr-x.net>
2018-01-07 02:07:42 +0100

    first commit

---
+++ Yap/YapView/Object.cs
@@ -0,0 +1,112 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace YapView
+{
+  public class Object
+  {
+    SKPaint objectPaint = new SKPaint()
+    {
+      Style = SKPaintStyle.StrokeAndFill,
+      Color = SKColors.AntiqueWhite,
+      
+    };
+
+    SKPaint objectBorderPaint = new SKPaint()
+    {
+      Style = SKPaintStyle.Stroke,
+      Color = SKColors.Black,
+    };
+
+    SKPaint objectBorderSelectedPaint = new SKPaint()
+    {
+      Style = SKPaintStyle.Stroke,
+      Color = SKColors.Green,
+    };
+
+    SKPaint greenPaint = new SKPaint()
+    {
+      Style = SKPaintStyle.Fill,
+      Color = SKColors.Green
+    };
+
+    SKPaint textPaint = new SKPaint()
+    {
+      Color = SKColors.Black,
+      Typeface = SKTypeface.FromFamilyName("consolas"),
+      TextSize = 12
+    };
+
+    SKRect rect;
+    SKSize size;
+
+    public Object(float left, float top)
+    {
+      rect = new SKRect();
+      size = new SKSize(100f, 20f);
+      
+      rect.Left = left;
+      rect.Top = top;
+      rect.Size = size;
+
+    }
+
+    String text = "";
+    public String Text
+    {
+      get => text;
+      set
+      {
+        text = value;
+        float width = 8 + textPaint.MeasureText(Text);
+        if (width < 80) width = 80;
+        Resize(width, size.Height);
+      }
+    }
+
+    public bool Selected { get; set; }
+    public bool EditMode { get; set; }
+
+    public bool IsInside(Point pos)
+    {
+      if(pos.X > rect.Left && pos.X < rect.Left + rect.Width)
+      {
+        if(pos.Y > rect.Top && pos.Y < rect.Top + rect.Height)
+        {
+          return true;
+        }
+      }
+      return false;
+    }
+    
+
+    public void Draw(SKCanvas canvas)
+    {
+      canvas.DrawRect(rect, objectPaint);
+      if(Selected)
+      {
+        canvas.DrawRect(rect, objectBorderSelectedPaint);
+      } else
+      {
+        canvas.DrawRect(rect, objectBorderPaint);
+      }
+
+      canvas.DrawLine(rect.Left, rect.Top + 3, rect.Right, rect.Top + 3, objectBorderPaint);
+      canvas.DrawLine(rect.Left, rect.Bottom - 3, rect.Right, rect.Bottom - 3, objectBorderPaint);
+
+      canvas.DrawText(Text, rect.Left + 4, rect.Top + 15, textPaint);
+    }
+
+    private void Resize(float width, float height)
+    {
+      size.Width = width;
+      size.Height = height;
+      rect.Size = size;
+    }
+  }
+}
---
+++ Yap/YapView/YapView.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SkiaSharp;
+using SkiaSharp.Views.Desktop;
+
+namespace YapView
+{
+  /// <summary>
+  /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+  ///
+  /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+  /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+  /// to be used:
+  ///
+  ///     xmlns:MyNamespace="clr-namespace:YapView"
+  ///
+  ///
+  /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+  /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+  /// to be used:
+  ///
+  ///     xmlns:MyNamespace="clr-namespace:YapView;assembly=YapView"
+  ///
+  /// You will also need to add a project reference from the project where the XAML file lives
+  /// to this project and Rebuild to avoid compilation errors:
+  ///
+  ///     Right click on the target project in the Solution Explorer and
+  ///     "Add Reference"->"Projects"->[Select this project]
+  ///
+  ///
+  /// Step 2)
+  /// Go ahead and use your control in the XAML file.
+  ///
+  ///     <MyNamespace:CustomControl1/>
+  ///
+  /// </summary>
+  public class YapView : SkiaSharp.Views.WPF.SKElement
+  {
+    static YapView()
+    {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(YapView), new FrameworkPropertyMetadata(typeof(YapView)));
+    }
+
+    SKPaint bluePaint = new SKPaint()
+    {
+      Style = SKPaintStyle.Fill,
+      Color = SKColors.Blue
+    };
+
+    Object o;
+    Object current = null;
+
+    float Width = 0f;
+    float Height = 0f;
+
+    StringEditor SEdit = new StringEditor();
+
+    public YapView()
+    {
+      o = new Object(100f, 100f);
+      o.Text = "Hello is a bit short";
+    }
+
+    protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
+    {
+      SKSurface surface = e.Surface;
+      SKCanvas canvas = surface.Canvas;
+
+      Width = e.Info.Width;
+      Height = e.Info.Height;
+
+      canvas.Clear(SKColors.DimGray);
+
+      o.Draw(canvas);
+    }
+
+    protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+    {
+      Point pos = e.GetPosition(this);
+      pos.X = pos.X / RenderSize.Width * Width;
+      pos.Y = pos.Y / RenderSize.Height * Height;
+      if(o.IsInside(pos))
+      {
+        if(o.Selected)
+        {
+          o.EditMode = true;
+          SEdit.Edit(current.Text, 0);
+        } else
+        {
+          o.Selected = true;
+        }
+        current = o;
+        InvalidateVisual();
+      } else
+      {
+        o.EditMode = false;
+        o.Selected = false;
+        current = null;
+        InvalidateVisual();
+      }
+      e.Handled = true;
+    }
+
+    protected override void OnKeyDown(KeyEventArgs e)
+    {
+      if(current != null && current.EditMode == true)
+      {
+        current.Text = SEdit.Update(e.Key);
+        InvalidateVisual();
+      }
+    }
+  }
+}
